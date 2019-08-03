c1576a889298118185076b28bc506a1c0992a453
Fernando Rosendo <franplt629@gmail.com>
2019-08-03 11:26:33 -0300

    checking for draws and clearing screen for Linux

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,7 @@
-//TODO
-//Check for draws
-//Clear screen 
-//Deal with errors
+// TODO
+// Check for draws //DONE
+// Clear screen //DONE
+// Deal with errors
 #include <iostream>
 
 using namespace std;
@@ -10,6 +10,7 @@ char winner();
 void play(char);
 void printBoard();
 void togglePlayer();
+bool draw();
 
 char board[][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 // char board[][3] ={'1','2','3','4','5','6','7','8','9'};
@@ -23,22 +24,54 @@ char symbol = 'X';
 // X goes first
 
 int main() {
+        system("clear");
         char choice = 0;
-        cout << "Player X starts!";
+        cout << "Player X starts!" << endl;
+        short total_plays = 0;
         while (!winner()) {
+                if (total_plays == 9) {
+                        system("clear");
+                        printBoard();
+                        cout << "Draw!" << endl;
+                        return 0;
+                }
                 cout << "Player " << symbol << "'s turn!" << endl;
                 cout << "What's the position you want to play at?" << endl;
                 cout << "Please say the correspondent number!" << endl;
                 printBoard();
                 cin >> choice;
                 play(choice);
+                total_plays += 1;
+                /*if (draw()){
+                        cout << "Draw!" << endl;
+                        return 0;
+                }*/
                 togglePlayer();
+                system("clear");
         }
 
+        system("clear");
+        printBoard();
         cout << "Player " << winner() << " wins!" << endl;
         system("pause");
         return 0;
 }
+/*
+bool draw() {
+        short total_plays = 0;
+        for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                        // checking if the element is not a number
+                        if (board[i][j] <= 48 || board[i][j] > 57)
+                                total_plays += 1;
+                }
+        }
+
+        if (total_plays == 9) return true;
+        return false;
+}
+*/
+// a smarter way to check the draw was done.
 void togglePlayer() {
         if (symbol == 'X')
                 symbol = 'O';
